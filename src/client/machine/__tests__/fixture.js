8dcb7ed3a83a9a0d6555c261564a6e25658ba629
Santiago Esteva <santiago.esteva@sabre.com>
2020-02-23 00:20:08 -0600

    update unit test to add more variations

--- src/client/machine/__tests__/fixture.js
+++ src/client/machine/__tests__/fixture.js
@@ -32,5 +32,191 @@ export const fixture = [
       "Google",
       "Razer"
     ]
+  },
+  {
+    "category":"History",
+    "type":"multiple",
+    "difficulty":"easy",
+    "question":"The idea of Socialism was articulated and advanced by whom?",
+    "correct_answer":"Karl Marx",
+    "incorrect_answers":[
+      "Vladimir Lenin",
+      "Joseph Stalin",
+      "Vladimir Putin"
+    ]
+  },
+  {
+    "category":"Animals",
+    "type":"text",
+    "difficulty":"medium",
+    "question":"What color\/colour is a polar bear&#039;s skin?",
+    "correct_answer":"Black"
+  },
+  {
+    "category":"Entertainment: Video Games",
+    "type":"multiple",
+    "difficulty":"easy",
+    "question":"If a &quot;360 no-scope&quot; is one full rotation before shooting, how many rotations would a &quot;1080 no-scope&quot; be?",
+    "correct_answer":"3",
+    "incorrect_answers":[
+      "4",
+      "2",
+      "5"
+    ]
+  },
+  {
+    "category":"Science: Mathematics",
+    "type":"multiple",
+    "difficulty":"hard",
+    "question":"How many zeptometres are inside one femtometre?",
+    "correct_answer":"1,000,000",
+    "incorrect_answers":[
+      "10",
+      "1,000,000,000",
+      "1000"
+    ]
+  },
+  {
+    "category":"Science: Mathematics",
+    "type":"boolean",
+    "difficulty":"easy",
+    "question":"The &#039;Squaring the Circle&#039; problem is solvable.",
+    "correct_answer":"False",
+    "incorrect_answers":[
+      "True"
+    ]
+  },
+  {
+    "category":"Entertainment: Music",
+    "type":"boolean",
+    "difficulty":"medium",
+    "question":"Ashley Frangipane performs under the stage name Halsey.",
+    "correct_answer":"True",
+    "incorrect_answers":[
+      "False"
+    ]
+  },
+  {
+    "category":"Entertainment: Video Games",
+    "type":"multiple",
+    "difficulty":"hard",
+    "question":"In &quot;Sonic the Hedgehog 3&quot; for the Sega Genesis, what is the color of the second Chaos Emerald you can get from Special Stages?",
+    "correct_answer":"Orange",
+    "incorrect_answers":[
+      "Blue",
+      "Green",
+      "Magenta"
+    ]
+  },
+  {
+    "category":"History",
+    "type":"multiple",
+    "difficulty":"medium",
+    "question":"When was &quot;The Gadget&quot;, the first nuclear device to be detonated, tested?",
+    "correct_answer":"July 16, 1945",
+    "incorrect_answers":[
+      "June 22, 1945",
+      "August 6, 1945",
+      "April 5, 1945"
+    ]
+  },
+  {
+    "category":"General Knowledge",
+    "type":"multiple",
+    "difficulty":"medium",
+    "question":"Which of the following buildings is example of a structure primarily built in the Art Deco architectural style?",
+    "correct_answer":"Niagara Mohawk Building",
+    "incorrect_answers":[
+      "Taipei 101",
+      "One Detroit Center",
+      "Westendstrasse 1"
+    ]
+  },
+  {
+    "category":"Entertainment: Board Games",
+    "type":"text",
+    "difficulty":"medium",
+    "question":"How many points is the Z tile worth in Scrabble?",
+    "correct_answer":"10"
+  },
+  {
+    "category":"Politics",
+    "type":"boolean",
+    "difficulty":"easy",
+    "question":"Denmark has a monarch.",
+    "correct_answer":"True",
+    "incorrect_answers":[
+      "False"
+    ]
+  },
+  {
+    "category":"Animals",
+    "type":"multiple",
+    "difficulty":"hard",
+    "question":"What is the scientific name of the Budgerigar?",
+    "correct_answer":"Melopsittacus undulatus",
+    "incorrect_answers":[
+      "Nymphicus hollandicus",
+      "Pyrrhura molinae",
+      "Ara macao"
+    ]
+  },
+  {
+    "category":"History",
+    "type":"multiple",
+    "difficulty":"easy",
+    "question":"When did the Battle of the Somme begin?",
+    "correct_answer":"July 1st, 1916",
+    "incorrect_answers":[
+      "August 1st, 1916",
+      "July 2nd, 1916",
+      "June 30th, 1916"
+    ]
+  },
+  {
+    "category":"Entertainment: Video Games",
+    "type":"boolean",
+    "difficulty":"easy",
+    "question":"Solid Snake is actually a clone from the DNA of Big Boss in the Metal Gear Solid series&#039; history.",
+    "correct_answer":"True",
+    "incorrect_answers":[
+      "False"
+    ]
+  },
+  {
+    "category":"Sports",
+    "type":"multiple",
+    "difficulty":"medium",
+    "question":"Which car manufacturer won the 2016 24 Hours of Le Mans?",
+    "correct_answer":"Porsche",
+    "incorrect_answers":[
+      "Toyota",
+      "Audi",
+      "Ferrari"
+    ]
+  },
+  {
+    "category":"Entertainment: Television",
+    "type":"multiple",
+    "difficulty":"easy",
+    "question":"How many seasons did &quot;Stargate SG-1&quot; have?",
+    "correct_answer":"10",
+    "incorrect_answers":[
+      "3",
+      "7",
+      "12"
+    ]
+  },
+  {
+    "category":"Entertainment: Music",
+    "type":"multiple",
+    "difficulty":"easy",
+    "question":"Which one of these songs did the group &quot;Men At Work&quot; NOT make?",
+    "correct_answer":"Safety Dance",
+    "incorrect_answers":[
+      "Down Under",
+      "Who Can It Be Now?",
+      "It&#039;s a Mistake"
+    ]
   }
 ]
--- src/client/machine/__tests__/quiz-machine.test.js
+++ src/client/machine/__tests__/quiz-machine.test.js
@@ -1,20 +1,24 @@
-import { interpret } from 'xstate'
+/**
+ * @jest-environment node
+ */
+
+ import { interpret } from 'xstate'
 import { quizMachine} from '../index'
 import { fixture } from './fixture'
 
 // http mocking
-const machine = quizMachine(2).withConfig({
+const machine = quizMachine(10).withConfig({
   services: { 
     getQuestions: (_, __) => async () => ({results: fixture}) 
   }
 })
 
 describe('Example of how to test the state machine', ()=>{
-  it('should get 2 questions and randomize them', (done)=>{
+  it('should get 10 questions and randomize them', (done)=>{
       const quiz = interpret(machine).onTransition(state => {       
         if(state.matches("question")){
-          expect(state.context.data).toHaveLength(2)
-          expect(state.context.data).not.toEqual(fixture.slice(0, 2) )
+          expect(state.context.data).toHaveLength(10)
+          expect(state.context.data).not.toEqual(fixture.slice(0, 10) )
           done()
         }
       }).start()  
